e213222b8a6e0415b7dd29ed1f5244630f071377
rishabh486 <76812565+rishabh486@users.noreply.github.com>
2021-10-04 20:00:49 +0530

    added footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+const Footer=props => {
+    return(
+        <footer class="footer">
+        <div>Connect me socially through</div>
+        <ul class="social-links list-non-bullet">
+            <li class="list-item-inline"><a class="link" href="https://www.linkedin.com/in/rishabh-jaiswal-78473421a/">LinkedIn</a></li>
+            <li class="list-item-inline"><a class="link" href="https://github.com/rishabh486">GitHub</a></li>
+            <li class="list-item-inline"><a class="link"href="https://twitter.com/rishabh27101">Twitter</a></li>
+        </ul>
+    </footer>
+    )
+}
+
+
+
+
+
+export default Footer
